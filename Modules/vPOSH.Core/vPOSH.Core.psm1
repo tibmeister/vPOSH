654b7fabf3b82df87c78114cc5ef6fb2ff39a679
Jody L. Whitlock <jody.whitlock@tiberiansun.us>
2019-09-24 19:47:27 -0500

    Some more refactoring

--- Modules/vPOSH.Core/vPOSH.Core.psm1
+++ Modules/vPOSH.Core/vPOSH.Core.psm1
@@ -13,7 +13,6 @@ function Initialize-Config
 
 Initialize-Config
 
-#TODO: Completly refactor this function
 Function Connect-vCenter
 {
     <#
@@ -522,7 +521,6 @@ Function Connect-ESXiHost
 	}
 }
 
-#TODO: Check this function before release
 Function Get-VMCPUReadyPercentDatacenter
 {
 	<#
@@ -573,7 +571,6 @@ Function Get-VMCPUReadyPercentDatacenter
 	return $output
 }
 
-#TODO: Check this function before release
 Function Get-VMCPUReadyPercentVM
 {
 	<#
@@ -624,7 +621,172 @@ Function Get-VMCPUReadyPercentVM
 	return $output
 }
 
-#TODO: Check this function before release
+#TODO: Implement this in a .1 release
+#region futurerelease
+# Function Watch-Output {
+#     <#
+#         .SYNOPSIS
+#             Runs a scriptblock or the preceeding pipeline repeatedly until there is change.
+
+#         .DESCRIPTION
+#             The Watch-Output cmdlet runs a specified scriptblock repeatedly at the specified interval (or
+#             every 1 second by default) and returns the result of the scriptblock when the output has changed.
+#             For the command to work the specified scriptblock must return a result to the pipeline.
+
+#         .PARAMETER ScriptBlock
+#             The scriptblock to execute, specified via curly braces. If you provide input via the pipleine that
+#             isn't a scriptblock then the entire invocation line that preceeded the cmdlet will be used as the
+#             scriptblock input.
+
+#         .PARAMETER Seconds
+#             Number of seconds to wait between checks. Default = 10
+
+#         .PARAMETER Difference
+#             Switch: Use to only output items in the collection that have changed
+#             dditions or modifications).
+
+#         .PARAMETER Continuous
+#             Switch: Run continuously (even after a change has occurred) until exited with CTRL+C.
+
+#         .PARAMETER AsString
+#             Switch: Converts the result of the scriptblock into an array of strings for comparison.
+
+#         .PARAMETER ClearScreen
+#             Switch: Clears the screen between each result. You can also use 'cls' as an alias.
+
+#         .PARAMETER Property
+#             Manually specify one or more property names to be used for comparison. If not specified,
+#             the default display property set is used. If there is not a default display property set,
+#             all properties are used. You can also use '*' to force all properties.
+
+#         .EXAMPLE
+#             Watch-Output -ScriptBlock { Get-Process }
+
+#             Runs Get-Process and waits for any returns the result when the data has changed.
+
+#         .EXAMPLE
+#             Get-Service | Watch-Output -Diff -Cont
+
+#             Runs Get-Service and returns any differences in the resultant data, continuously until interrupted
+#             by CTRL+C.
+
+#         .EXAMPLE
+#             Watch-Output { Get-Content test.txt } -Difference -Verbose -ClearScreen
+
+#             Uses Get-Content to monitor test.txt. Shows any changes and clears the screen between changes.
+
+#         .EXAMPLE
+#             Get-ChildItem | Watch-Output -Difference -AsString
+
+#             Monitors the result of GEt-ChildItem for changes, returns any differences. Treats the input as
+#             strings not objects.
+
+#         .EXAMPLE
+#             Get-Process | Watch-Output -Difference -Property processname,id -Continuous
+
+#             Monitors Get-Process for differences in the specified properties only, continues until interrupted
+#             by CTRL+C.
+#     #>
+#     [cmdletbinding()]
+#     Param(
+#         [parameter(ValueFromPipeline, Mandatory)]
+#         [object]
+#         $ScriptBlock,
+
+#         [int]
+#         $Seconds = 10,
+
+#         [switch]
+#         $Difference,
+
+#         [switch]
+#         $Continuous,
+
+#         [switch]
+#         $AsString,
+
+#         [alias('cls')]
+#         [switch]
+#         $ClearScreen,
+
+#         [string[]]
+#         $Property
+#     )
+
+#     if ($ScriptBlock -isnot [scriptblock]) {
+#         if ($MyInvocation.PipelinePosition -gt 1) {
+#             $ScriptBlock = [Scriptblock]::Create( ($MyInvocation.Line -Split "\|\s*$($MyInvocation.InvocationName)")[0] )
+#         }
+#         else {
+#             Throw 'The -ScriptBlock parameter must be provided an object of type ScriptBlock unless invoked via the Pipeline.'
+#         }
+#     }
+
+#     Write-Verbose "Started executing $($ScriptBlock | Out-String)"
+
+#     $FirstResult = Invoke-Command $ScriptBlock
+
+#     if ($AsString) {
+#         $FirstResult = $FirstResult | Out-String -Stream
+#     }
+#     elseif (($FirstResult | Select-Object -First 1) -isnot [string]){
+#         if (-not $Property) {
+#             $Property = ($FirstResult | Select-Object -First 1).PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames
+#         }
+
+#         if (-not $Property -or $Property -eq '*') {
+#             $Property = ($FirstResult | Select-Object -First 1).PSObject.Properties.Name
+#         }
+
+#         Write-Verbose "Watched properties: $($Property -Join ',')"
+#     }
+
+
+#     do {
+#         do {
+#             if ($Result) {
+#                 Start-Sleep $Seconds
+#             }
+
+#             if ($ClearScreen) {
+#                 Clear-Host
+#             }
+
+#             $Result = Invoke-Command $ScriptBlock
+
+#             if ($AsString) {
+#                 $Result = $Result | Out-String -Stream
+#             }
+
+#             $CompareParams = @{
+#                 ReferenceObject  = @($FirstResult | Select-Object)
+#                 DifferenceObject = @($Result | Select-Object)
+#             }
+
+#             if ($Property) {
+#                 $CompareParams.Add('Property', $Property)
+#             }
+
+#             $Diff = Compare-Object @CompareParams -PassThru
+#         }
+#         until ($Diff)
+
+#         Write-Verbose "Change occurred at $(Get-Date)"
+
+#         if ($Difference) {
+#             $Diff | Where-Object {$_.SideIndicator -eq '=>'}
+#         }
+#         else {
+#             $Result
+#         }
+
+#         $FirstResult = $Result
+#     }
+#     until (-not $Continuous)
+# }
+#endregion
+
+#TODO: Add in last update at top of screen
 Function Watch-Command
 {
 	<#
@@ -648,6 +810,7 @@ Function Watch-Command
 		ValueFromPipelineByPropertyName=$true,
 		HelpMessage='Command to run in the form of a script block')]
 		[scriptblock]$CommandToRun,
+
 		[Parameter(Mandatory=$false,
 		ValueFromPipeline=$true,
 		ValueFromPipelineByPropertyName=$true,
@@ -708,7 +871,6 @@ Function Watch-Command
 	}
 }
 
-#TODO: Check this function before release
 Function Start-RollingReboot
 {
 	<#
@@ -790,7 +952,6 @@ Function Start-RollingReboot
 	}
 }
 
-#TODO: Check this function before release
 Function Wait-VMShutdown
 {
 	<#
@@ -866,7 +1027,6 @@ Function Wait-VMShutdown
 	}
 }
 
-#TODO: Check this function before release
 Function Get-vCenterSessions
 {
     <#
@@ -920,7 +1080,6 @@ Function Get-vCenterSessions
     return $AllSessions
 }
 
-#TODO: Check this function before release
 function Get-LastPowerOn
 {
 	<#
